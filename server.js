d491024477d09af5db3caeff8040a6a793a7562f
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.attlocal.net>
2022-02-01 09:35:10 -0800

    added cookie parser

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express")
 const cors = require("cors")
 
 require('dotenv').config(); //so that we can read the information about our secret key which is stored in the .env file
-
+const cookieParser = require('cookie-parser'); //so that the server can understand the cookie information coming in from the client (browser)
 
 const app = express(); 
 const port = 8000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cookieParser());
 
 
 
